78483ab82d2daa4ab7ee6056d6aae90b9d13a6b3
kitakkun <48154936+kitakkun@users.noreply.github.com>
2022-04-22 12:24:08 +0900

    add touch event handling
    for interactions on mobile devices

--- Othello/Assets/Scripts/GameSystem/CellSelector.cs
+++ Othello/Assets/Scripts/GameSystem/CellSelector.cs
@@ -14,18 +14,32 @@ namespace GameSystem
                 .Where(_ => Input.GetMouseButtonDown(0))
                 .Subscribe(_ =>
                 {
-                    RaycastHit hit;
-                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0.5f, false);
-                    if (Physics.Raycast(ray, out hit, 100))
-                    {
-                        var obj = hit.collider.gameObject;
-                        var cell = obj.GetComponent<BoardCell>();
-                        Debug.Log($"Hit: x = {cell.X} y = {cell.Y}");
-                        _board.PutDisc(cell.X + Board.CellSize * cell.Y, _board.Turn);
-                    }
+                    PutDisc(Input.mousePosition);
                 })
                 .AddTo(this);
+            this.UpdateAsObservable()
+                .Where(_ => Input.touchSupported)
+                .Where(_ => Input.touches.Length > 0)
+                .Select(_ => Input.GetTouch(0))
+                .Subscribe(touch =>
+                {
+                    PutDisc(touch.position);
+                })
+                .AddTo(this);
+        }
+
+        void PutDisc(Vector3 position)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(position);
+            Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0.5f, false);
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+               var obj = hit.collider.gameObject;
+               var cell = obj.GetComponent<BoardCell>();
+               Debug.Log($"Hit: x = {cell.X} y = {cell.Y}");
+               _board.PutDisc(cell.X + Board.CellSize * cell.Y, _board.Turn);
+            } 
         }
     }
 }
